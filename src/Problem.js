027306aedbae98b71c6640437eff4aea708750da
Kacper Swietlaga <kacperres@gmail.com>
2018-12-13 18:27:02 +0100

    added and prepear Problem component

---
+++ src/Problem.js
@@ -0,0 +1,75 @@
+import React from 'react'
+
+class Problem extends React.Component {
+    state = {
+        isEdited: false,
+        problemTitle: this.props.problem.title,
+        problemKeyWords: this.props.problem.keyWords,
+        problemDescription: this.props.problem.description
+    }
+    editToggle = () => {
+        isEdited = !this.state.isEdited,
+            problemTitle = this.props.problem.title,
+            problemKeyWords = this.props.problem.keyWords,
+            problemDescription = this.props.problem.description
+    }
+    onProblemChangeHandler = (event) => {
+        this.setState({
+            problemTitle = this.props.problem.title,
+            problemKeyWords = this.props.problem.keyWords,
+            problemDescription = this.props.problem.description
+        })
+    }
+    render() {
+        return (
+            this.state.isEdited ?
+                <div>
+                    <button
+                        onClick={this.editToggle}
+                    >
+                        Canel
+                </button>
+                    <input
+                        type={'text'}
+                        onChange={this.onProblemChangeHandler}
+                        value={this.state.problemTitle}
+                    />
+                    <input
+                        type={'text'}
+                        onChange={this.onProblemChangeHandler}
+                        value={this.state.problemDescription}
+                    />
+                    <input
+                        type={'text'}
+                        onChange={this.onProblemChangeHandler}
+                        value={this.state.problemKeyWords}
+                    />
+                    <button
+                        onClick={
+                            ()=>{
+                                this.props.onEditProblemHandler(
+                                    this.props.problem.key,
+                                    this.state.problemTitle,
+                                    this.state.problem.description,
+                                    this.state.problem.keyWords
+                                )
+                                this.editToggle()
+                            }
+                        }
+                    >
+                        SAVE
+                    </button>
+                </div>
+                :
+                <div>
+                    {this.props.problem.title}
+                    <button
+                        onClick={this.editToggle}
+                    >
+                        EDIT
+                    </button>
+                </div>
+        )
+    }
+}
+export default Problem
