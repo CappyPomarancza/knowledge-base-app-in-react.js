52ebb0cebf2c553987f25a27a4f571c14dadcda7
Kacper Swietlaga <kacperres@gmail.com>
2018-12-24 16:17:12 +0100

    added Default component

---
+++ src/ProblemList/Default.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+
+const Default = (props) => (
+    <div>
+        <button
+            onClick={props.clickHandler}
+        >
+        wft?
+            {props.label}
+        </button>
+    </div>
+)
+
+
+Default.propTypes ={
+    clickHandler: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired
+}
+
+export default Default
