07d9a9fa6897eb8598b95360fded7d2b77db5a5f
Kacper Swietlaga <kacperres@gmail.com>
2018-12-24 16:28:59 +0100

    prepare ProblemList component

--- src/ProblemList/ProblemsList.js
+++ src/ProblemList/ProblemsList.js
@@ -1,1 +1,46 @@
 import React from 'react'
+import Default from './Default'
+import Loading from './Loading'
+import List from './List'
+import mapObjectToArray from '../untils'
+import Forms from './Forms'
+//import Shearch from './Schearch'
+
+//import { database } from '../firebaseConfig'
+
+class ProblemsList extends React.Component {
+    state = {
+        problems: null,
+        isLoadingProblems: false,
+        newProblemTitle: '',
+        searchPharse: '',
+    }
+
+    initProblemsSync = () => {
+        this.setState({
+            isLoadingProblems: true
+        })
+        database.ref(adresURl)
+            .on(
+                'value',
+                snapshot => {
+                    const data = snapshot.val()
+                    this.setState({
+                        problems: mapObjectToArray(data),
+                        isLoadingProblems: false,
+                    })
+                }
+            )
+    }
+
+    newProblemChandler = (event) => {
+        this.setState({
+            newProblemTitle: event.target.value,
+            newProblemKeyWords: event.target.value,
+            newProblemDescription: event.target.value
+        })
+    }
+
+
+
+}
