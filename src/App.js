d4ffb45b1871175e65bb09d8124099ceed1cf7e4
Kacper Swietlaga <kacperres@gmail.com>
2018-12-25 13:30:35 +0100

    added ProblemList component to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
-import Problem from './Problem'
+import Problem from './ProblemList/Problem'
 
 class App extends Component {
   render() {
--- src/ProblemList/ProblemsList.js
+++ src/ProblemList/ProblemsList.js
@@ -33,7 +33,7 @@ class ProblemsList extends React.Component {
             )
     }
 
-    newProblemChandler = (event) => {
+    newProblemChandlerHandler = (event) => {
         this.setState({
             newProblemTitle: event.target.value,
             newProblemKeyWords: event.target.value,
@@ -41,6 +41,60 @@ class ProblemsList extends React.Component {
         })
     }
 
+    onAddNewProblemClick = () => {
+        const request = {
+            method: 'POST',
+            body: JSON.stringify({
+                title: this.state.newProblemTitle,
+                keyWords: this.state.newProblemKeyWords,
+                description: this.state.newProblemDescription
+            })
+        }
+
+        fetch('https://to-do-list-aceb0.firebaseio.com/cappy-problems/.json',
+            request)
+            .then(response => {
+                this.setState({
+                    newProblemTitle: '',
+                    newProblemDescription: '',
+                    newProblemKeyWords: ''
+                })
+            })
+    }
+    onEditProblemHandler = (key, newTitle) => {
+        database.ref(`cappy-problems/${key}`)
+            .update({
+                title: newTitle
+            })
+    }
+    onShearchPharseChanged = (event) => {
+        this.setState({
+            searchPharse: event.target.value
+        })
+    }
+
+    render(){
+        const filteredProblems = this.state.problems && this.state.problems
+        .filter(problem => problem.title.idexOf(this.state.searchPharse)!== -1)
+
+        return(
+            <div>
+                {
+
+                    'dupa'
+                    // this.state.isLoadingProblems?
+                    // <Loading />
+                    // :
+                    // this.state.problems ?
+                    // <div>
+                    //     <Forms 
+                    //         newProblemTitle = {this.state.newProblemTitle}
+                    //     />
+                    // </div>
+                }
+            </div>
+        )
+    }
 
 
 }
