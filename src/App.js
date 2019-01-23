995fa18b36dcfacde7d3898a989f8928d5a569fb
Kacper Swietlaga <kacperres@gmail.com>
2019-01-23 21:20:12 +0100

    deleted navigation- sideBar and AppBar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css'
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import ProblemsList from './ProblemList/ProblemsList'
-import Navigation from './Navigation'
 import Forms from './ProblemList/Forms';
 import Search from './ProblemList/Search';
 
@@ -12,18 +11,7 @@ class App extends Component {
   render() {
     return (
       <div>
-
-
         <ProblemsList />
-
-
-
-
-        {/* <Navigation />
-    <Route path={'/forms'} component={Forms} />
-      <Route path={'/problem-list'} component={ProblemsList} />
-      <Route path={'/search'} component={Search} /> */}
-
       </div>
     )
   }
